a2ff700478247bd3edc3048d1d36328379da2f62
mksparky <73658539+maxkraus1@users.noreply.github.com>
2021-11-16 15:52:53 -0800

    Added bulk_enroll_dept.py

---
+++ bulk_enroll_dept.py
@@ -0,0 +1,34 @@
+"""Enrolls a user in all courses in a department where they are not enrolled"""
+
+import argparse
+import os
+import pandas as pd
+
+import datahub
+import dwnld
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dept', type=str, help='department code', required=True)
+parser.add_argument('--sem', type=int, help='semester code', required=True)
+parser.add_argument('--id', type=str, help='user id (LMS Org Defined Id)', required=True)
+parser.add_argument('--role', type=str, help='role id')
+args = parser.parse_args()
+
+user = datahub.get_user(args.id)
+
+semester_report = os.path.join( datahub.REPORT_PATH,
+                                "Semester Course Reports",
+                                "{}_SemesterReport.xlsx".format(args.sem))
+
+df = pd.read_excel(semester_report)
+df = df[df['DeptCode'] == args.dept]
+df = df[df['InstructorXnumber'].str.contains(args.id) == False]
+df.dropna(subset=['InstructorXnumber'], inplace=True)
+
+if args.role:
+    role = args.role  # if role argument is supplied, assign to variable
+else:
+    role = 122  # default role is Department Staff
+
+for ou in df['OrgUnitId'].to_list():
+    dwnld.enroll(ou, user['UserId'], role)
