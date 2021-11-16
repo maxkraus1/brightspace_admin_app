630c136b4bb2c3a2b1926bdf6dde8c007df2d9c4
mksparky <73658539+maxkraus1@users.noreply.github.com>
2021-11-16 11:32:21 -0800

    Added methods for semester and grades report to app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 """Provides an API endpoint at the localhost for communication with Postman"""
 
 import json
+import os
 
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
@@ -17,5 +18,27 @@ class Credentials(Resource):
 
 api.add_resource(Credentials, '/credentials')
 
+@app.route('/')
+def form():
+    processes = ['Semester Report', 'Grades Report']
+    return render_template('form.html', processes=processes)
+
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        os.system('python semester_report.py {}'.format(form_data['SemesterCode']))
+        return render_template('data.html',form_data = form_data)
+
+@app.route('/grades_report/', methods=['POST'])
+def grades_report():
+    form_data = request.form
+    os.system('python grades_report.py --org {}'.format(form_data['OrgUnitId']))
+    return render_template('data.html',form_data = form_data)
+
 if __name__ == '__main__':
-    app.run()
+    # import webbrowser
+    # webbrowser.open('http://localhost:5000')
+    app.run(port=5000)
