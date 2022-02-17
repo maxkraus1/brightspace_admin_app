f1dc0bae848e0c5a0da1cb597304457082aec182
mksparky <73658539+maxkraus1@users.noreply.github.com>
2022-02-17 14:55:26 -0800

    replaced standard csv reader with pandas.read_csv()

--- evidence.py
+++ evidence.py
@@ -3,10 +3,11 @@
 Takes a keyphrase as argument 1 and csv file as argument 2
 """
 
-import csv
 import os
 import re
 import sys
+
+import pandas as pd
 
 import datahub
 import dwnld
@@ -17,26 +18,25 @@ csvfile = sys.argv[2]
 no_course = []
 downloaded = []
 
-with open(csvfile, newline="") as infile:
-    reader = csv.DictReader(infile)
-    for row in reader:
-        orgunit = row["OrgUnitId"]
-        name = re.sub(r'\W+', '', row["Name"])
-        coursepath = os.path.join(row["Path"], "{}_{}".format(row["Code"], name))
-        try:
-            if os.path.isdir(coursepath) == False:
-                try:
-                    os.mkdir(coursepath)
-                except OSError as error:
-                    print(error)
-            dwnld.get_files(orgunit, keyphrase, coursepath)
-        except:
-            no_course.append(row)
-        if len(os.listdir(coursepath)) == 0:
-            os.rmdir(coursepath)
-            print("No files in {}".format(row["Name"]))
-        else:
-            downloaded.append(os.path.basename(os.path.normpath(row["Path"])))
+reader = pd.read_csv(csvfile, dtype=str)
+for index, row in reader.dropna().iterrows():
+    orgunit = row["OrgUnitId"]
+    name = re.sub(r'\W+', '_', row["Name"])
+    coursepath = os.path.join(row["Path"], "{}_{}".format(row["Code"], name))
+    try:
+        if os.path.isdir(coursepath) == False:
+            try:
+                os.mkdir(coursepath)
+            except OSError as error:
+                print(error)
+        dwnld.get_files(orgunit, keyphrase, coursepath)
+    except:
+        no_course.append(row)
+    if len(os.listdir(coursepath)) == 0:
+        os.rmdir(coursepath)
+        print("No files in {}".format(row["Name"]))
+    else:
+        downloaded.append(os.path.basename(os.path.normpath(row["Path"])))
 if no_course:
     print("Courses below were not found:")
     for row in no_course:
